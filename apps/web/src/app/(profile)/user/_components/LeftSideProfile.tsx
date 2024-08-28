083239d47fbedfb09e326754b338626a8ce786ef
RidloGhifary <ridloghfry@gmail.com>
2024-08-28 20:02:15 +0700

    style: added icons for left side profile

--- apps/web/src/app/(profile)/user/_components/LeftSideProfile.tsx
+++ apps/web/src/app/(profile)/user/_components/LeftSideProfile.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import { MdEmail } from "react-icons/md";
+import { BsGenderFemale, BsGenderMale } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
 
 export default function LeftSideProfile() {
+  const gender = "male";
+
   return (
     <div className="mt-4 hidden w-[300px] space-y-4 rounded-md p-4 shadow md:block">
       <Image
@@ -12,9 +17,17 @@ export default function LeftSideProfile() {
         className="aspect-square rounded-full object-cover"
       />
       <div className="space-y-1 text-sm">
-        <p className="line-clamp-1 truncate">Ridlo achmad ghifary</p>
-        <p className="line-clamp-1 truncate">ridloach@gmail.com</p>
-        <span className="badge badge-primary text-white">male</span>
+        <div className="line-clamp-1 flex items-center gap-1 truncate">
+          <span className="badge badge-primary text-white">{gender}</span>
+        </div>
+        <div className="line-clamp-1 flex items-center gap-1 truncate">
+          <FaUser className="text-primary" size={20} />
+          <span>Ridlo achmad ghifary</span>
+        </div>
+        <div className="line-clamp-1 flex items-center gap-1 truncate">
+          <MdEmail className="text-primary" size={20} />
+          <span>ridloach@gmail.com</span>
+        </div>
       </div>
     </div>
   );
--- apps/web/src/app/loading.tsx
+++ apps/web/src/app/loading.tsx
@@ -3,7 +3,7 @@ import { PulseLoader } from "react-spinners";
 export default function Loading() {
   return (
     <div className="flex h-[50vh] items-center justify-center">
-      <PulseLoader color="#36d7b7" size={50} />
+      <PulseLoader color="#36d7b7" size={30} />
     </div>
   );
 }

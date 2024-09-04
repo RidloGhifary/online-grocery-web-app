b6f900e20f3dd332c77a37f570a220db21004252
RidloGhifary <ridloghfry@gmail.com>
2024-09-04 12:44:45 +0700

    fix: middleware to protect route

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,43 +1,67 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { jwtDecode, JwtPayload } from "jwt-decode";
 
 const protectedRoutes = [
   "/user",
-  "/user/settings",
-  "/user/address",
   "/cart",
   "/my-vouchers",
   "/checkout",
   "/transaction",
 ];
-const superAdminRoutes = ["/create-store"];
+const superAdminRoutes = ["/create-store", "/admin"];
 const authRoutes = ["/login", "/register", "/verify-account"];
 
 export default function middleware(req: NextRequest) {
   const token = cookies().get("token")?.value;
-  const isLoggedIn = !!token;
 
-  const path = req.nextUrl.pathname;
-  const isProtectedRoute = protectedRoutes.some((route) =>
-    path.startsWith(route),
-  );
-  const isAuthRoute = authRoutes.some((route) => path.startsWith(route));
-  const isSuperAdminRoute = superAdminRoutes.some((route) =>
-    path.startsWith(route),
-  );
+  let decoded: (JwtPayload & { email?: string }) | null = null;
+  let isSuperAdmin = false;
+  let isLoggedIn = false;
 
-  if (!isLoggedIn && isProtectedRoute) {
-    const callbackUrl = req.nextUrl.search
-      ? `${path}${req.nextUrl.search}`
-      : path;
-    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
-    const redirectUrl = `/login?callbackUrl=${encodedCallbackUrl}`;
-
-    return NextResponse.redirect(new URL(redirectUrl, req.url));
+  try {
+    if (token) {
+      decoded = jwtDecode<JwtPayload & { email: string }>(token);
+      isLoggedIn = true;
+      isSuperAdmin = decoded.email?.includes("super.admin") || false;
+    }
+  } catch (error) {
+    console.error("JWT decoding error:", error);
   }
 
-  if (isLoggedIn && isAuthRoute) {
-    return NextResponse.redirect(new URL("/user/settings", req.url));
+  const path = req.nextUrl.pathname;
+
+  if (!isLoggedIn) {
+    // Deny access to protected and superAdminRoutes
+    if (
+      protectedRoutes.some((route) => path.startsWith(route)) ||
+      superAdminRoutes.some((route) => path.startsWith(route))
+    ) {
+      const callbackUrl = req.nextUrl.search
+        ? `${path}${req.nextUrl.search}`
+        : path;
+      const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+      const redirectUrl = `/login?callbackUrl=${encodedCallbackUrl}`;
+      return NextResponse.redirect(new URL(redirectUrl, req.url));
+    }
+  } else if (isSuperAdmin) {
+    // Redirect SuperAdmins away from authRoutes and protectedRoutes
+    if (authRoutes.some((route) => path.startsWith(route))) {
+      return NextResponse.redirect(new URL("/admin", req.url));
+    }
+
+    if (protectedRoutes.some((route) => path.startsWith(route))) {
+      return NextResponse.redirect(new URL("/admin", req.url));
+    }
+  } else {
+    // Regular users handling
+    if (superAdminRoutes.some((route) => path.startsWith(route))) {
+      return NextResponse.redirect(new URL("/user/settings", req.url)); // Redirect regular users from superAdminRoutes
+    }
+
+    if (authRoutes.some((route) => path.startsWith(route))) {
+      return NextResponse.redirect(new URL("/", req.url)); // Redirect logged-in users from authRoutes
+    }
   }
 
   return NextResponse.next();
